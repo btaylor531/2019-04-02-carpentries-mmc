9e0f1aeb9ea12d4d8e49e0a2b282b520a7f07fb2
btaylor531 <bjtaylor@mmc.org>
2019-04-02 14:52:23 +0000

    get gapminder data

---
+++ src/data/GapMinder/gapminder.R
@@ -0,0 +1,6 @@
+# Download the gapminder data for analysis
+#
+#
+library(here)
+
+file_url <- "https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv"
